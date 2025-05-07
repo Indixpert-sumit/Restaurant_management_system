ea09cd8b5f6ae3d5031ede33d83f3dec2107bfa9
Indixpert-sumit <sy0050854@gmail.com>
2025-05-07 12:38:20 +0530

    menu_handling done

--- Domain/menu_handling.py
+++ Domain/menu_handling.py
@@ -45,24 +45,25 @@ class Adding_items(Menu_items):
         menu[name]=price
         print("food name",menu)
         print("price",price)
-        print("successfully added in menu ")
+        print(f"{name}successfully added in menu ")
         
-        print(menu)     
 
 class Delete_iteams(Menu_items):
     def __init__(self):
         pass
     
     def delete_iteams(self):
-        self.menu_item()
         menu = {"BLACK COFFEE": 50, "GREEN TEA": 50, "GRAVVY MOMOS": 110,"SANDWICH": 70, "BEEF SAUSAGE": 140, "CHICKEN STEAK": 160}
+        
 
-        name=input('Enter food name you want to remove ')
-        print(name,"SUCCESSFULLY REMOVED ")
+        remove= input('Enter food name you want to remove: ')
+
+        if remove in menu:
+            menu.pop(remove)
+            print(f"{remove} SUCCESSFULLY REMOVED")
+        else:
+            print(f"{remove} is not found in the menu!")
         print(menu)
-        name=menu.pop(name)
-        print("\t\t\t MENU UPDATED",menu)
-
 
        
 
